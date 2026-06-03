6435635f5e93fe321d4a6f0d266855ce106d30fd
Masbah <hayamahmed363@gmail.com>
2026-06-04 01:56:35 +0300

    Initialize main.dart with necessary imports

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vibration/vibration.dart';
+import 'dart:math';
+
+void main() {
+  WidgetsFlutterBinding.ensureIniti
